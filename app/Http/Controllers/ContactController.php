429ec83526a7d179741ae3008e8a2f9afb5e4f5f
hidel21 <hidelmartinez21@gmail.com>
2023-03-15 21:48:05 -0300

    Allow Sorts

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -15,7 +15,7 @@ class ContactController extends Controller
 
         // DB::enableQueryLog();
         $contacts = Contact::allowedTrash()
-            ->allowedSorts('first_name')
+            ->allowedSorts(['first_name', 'last_name', 'email'], "-id")
             ->allowedFilters('company_id')
             ->allowedSearch('first_name', 'last_name', 'email')
             ->paginate(10);
